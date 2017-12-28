c36a027b26b6c7ad75569bc0af4f9b361ef746cf
nartov <nartov.km@gmail.com>
2017-12-28 16:31:37 +0300

    easy part solved

--- 04-lesson/04-lesson-easy.py
+++ 04-lesson/04-lesson-easy.py
@@ -14,9 +14,26 @@ print(list(x ** 2 for x in initial_list))
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
 
+# apple orange peach pineapple kiwi
+# pineapple orange lemon banana mandarin
+
+first_list = list(input('Введите первый список фруктов через пробел: ').split())
+second_list = list(input('Введите второй список фруктов через пробел: ').split())
+
+common_list = [elem for elem in second_list if elem in first_list]
+
+print(common_list)
 # Задание-3:
 # Дан список, заполненный произвольными числами.
 # Получить список из элементов исходного, удовлетворяющих следующим условиям:
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+
+
+initial_list = map(lambda x: int(x), list(input('Введите числа через пробел: ').split()))
+final_list = [elem for elem in initial_list if elem >= 0 and elem % 3 == 0 and elem % 4 != 0]
+print(final_list)
+
+
