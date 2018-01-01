08fa3e8dd934bba728a969f2a6282cd34a858113
nartov <nartov.km@gmail.com>
2018-01-02 00:05:01 +0300

    Homework attached, first ex  in easy solved

--- 05-lesson/hw05_easy.py
+++ 05-lesson/hw05_easy.py
@@ -9,7 +9,6 @@ create_dir = input('Хотите создать папки dir_1 dir_9? [Y/N]').
 
 if create_dir == 'Y':
     for x in range(1, 10):
-        dir_list.append('dir_' + str(x))
         try:
             os.mkdir('dir_' + str(x))
         except FileExistsError:
@@ -19,17 +18,31 @@ else:
 
 delete_dir = input('Хотите удалить папки dir_1 dir_9?').upper()
 
-dir_list = os.listdir()
+directory_list = os.listdir()
+
 if delete_dir == 'Y':
-    for x in dir_list:
+    for x in directory_list:
         if os.path.isdir(x):
-            os.rmdir(x)
-        else:
-            pass
+            try:
+                os.rmdir(x)
+            except OSError:
+                print('Папка {} не пустая, папка не удалена'.format(x))
+
 
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+
+def dir_list():
+    list_of_dir = sorted(os.listdir())
+    for elem in list_of_dir:
+        if os.path.isdir(elem):
+            print(elem + '\n')
+        else:
+            pass
+
+
+dir_list()
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
