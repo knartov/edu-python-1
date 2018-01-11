4c60ac9efe423a541da996b8a07eb74bb9af86ea
nartov <nartov.km@gmail.com>
2018-01-11 16:33:34 +0300

    Homework attached, easy solved

--- 05-lesson/hw05_normal.py
+++ 05-lesson/hw05_normal.py
@@ -10,6 +10,52 @@
 # и выводит результат действия: "Успешно создано/удалено/перешел",
 # "Невозможно создать/удалить/перейти"
 
+import os
+from easy import dir_list
+
+
+def main():
+    print("Python util")
+
+    answer = ''
+
+    while answer != 'q':
+        answer = input("Давайте поработаем? (Y/N/q)")
+        if answer == 'Y':
+            print("Отлично, хозяин!")
+            print("Я умею:")
+            print(" [1] - Перейти в папку")
+            print(" [2] - Просмотреть содержимое текущей папки")
+            print(" [3] - Удалить папку")
+            print(" [4] - Создать папку")
+
+            do = int(input("Укажите номер действия: "))
+
+            if do == 1:
+                print(os.listdir())
+
+            elif do == 2:
+                print()
+
+            elif do == 3:
+                print(psutil.pids())
+
+            elif do == 4:
+                print("=Дублирование файлов в текущей директории=")
+                duble_files('.')
+
+            else:
+                pass
+
+        elif answer == 'N':
+            print("До свидания!")
+            break
+        else:
+            print("Неизвестный ответ")
+
+
+if __name__ == "__main__":
+    main()
 
 
 # Для решения данной задачи используйте алгоритмы из задания easy,
